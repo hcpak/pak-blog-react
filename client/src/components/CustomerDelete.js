a926a100e6f8d0b5490581c38232e7d378fa4749
hyungchul pak <jasotn12@naver.com>
2020-09-08 00:33:31 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CustomerDelete extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    handleClickOpen = () =>{
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false,
+        })
+    }
     deleteCustomer(id){
         const url = '/api/customers/' +id;
         fetch(url, {
@@ -10,7 +33,23 @@ class CustomerDelete extends React.Component{
     }
     render(){
         return (
-            <button onClick={(e) =>{this.deleteCustomer(this.props.id)}}> delete</button>
+            <div>
+                <Button variant = "contained" color = "secondary" onClick={this.handleClickOpen}> delete</Button>
+                <Dialog open={this.state.open} onClose ={this.handleClose}>
+                    <DialogTitle onClose  ={this.handleClose}>
+                        delete 
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            selected customer information will be deleted.
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick ={(e) => {this.deleteCustomer(this.props.id)}}>delete</Button>
+                        <Button variant= "outlined" color="primary" onClick ={this.handleClose}>close</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         )
     }
 }
